9daaba3f9976cbe743c261c65cb06cc33f701dd1
ksimoens <kobe.simoens@ugent.be>
2022-10-05 17:07:39 +0200

    change data files

--- distancesFst.R
+++ distancesFst.R
@@ -1,17 +1,28 @@
 library(adegenet)
 library(diveRsity)
 
-data <- read.genepop("Input/lobster_1278ind_79snps_40pop.gen")
+calcFst <- function(gendata){
 
-diffstats <- diffCalc("Input/lobster_1278ind_79snps_40pop.gen", fst=T, pairwise=T, bs_locus=F, bs_pairwise=F)
+	diffstats <- diffCalc(gendata, fst=T, pairwise=T, bs_locus=F, bs_pairwise=F)
 
-Fst <- diffstats$pairwise$Fst
+	Fst <- diffstats$pairwise$Fst
 
-Fst.dist <- as.dist(Fst, diag=T, upper=T)
+	Fst.dist <- as.dist(Fst, diag=T, upper=T)
 
-Fst.mat <- as.matrix(Fst.dist)
-rownames(Fst.mat) <- substr(rownames(Fst.mat),1,3)
-colnames(Fst.mat) <- substr(colnames(Fst.mat),1,3)
-Fst.mat[Fst.mat < 0] <- 0
+	Fst.mat <- as.matrix(Fst.dist)
+	rownames(Fst.mat) <- substr(rownames(Fst.mat),1,3)
+	colnames(Fst.mat) <- substr(colnames(Fst.mat),1,3)
+	Fst.mat[Fst.mat < 0] <- 0
 
-write.csv(Fst.mat,"Output/distancesFst.csv")
+	return(Fst.mat)
+
+}
+
+Fst.tot <- calcFst("Input/lobster_1278ind_79snps_40pop.gen")
+write.csv(Fst.tot,"Output/distancesFst_tot.csv")
+
+Fst.sel <- calcFst("Input/lobster_1278ind_8snps_40pop.gen")
+write.csv(Fst.sel,"Output/distancesFst_sel.csv")
+
+Fst.neut <- calcFst("Input/lobster_1278ind_71snps_40pop.gen")
+write.csv(Fst.neut,"Output/distancesFst_neut.csv")
