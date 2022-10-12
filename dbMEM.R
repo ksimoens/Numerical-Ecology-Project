6b2129849d7eafb336eab85c29212f00a50cf87f
ksimoens <kobe.simoens@ugent.be>
2022-10-12 14:07:00 +0200

    dbMEM

---
+++ dbMEM.R
@@ -0,0 +1,11 @@
+library(adespatial)
+
+distances <- read.csv("Output/distances.csv",header=T,row.names=1)
+
+distances <- distances[!(row.names(distances) %in% c("Laz","Tar","Sar","Ale","The","Tor","Sky")),]
+distances <- distances[,!(names(distances) %in% c("Laz","Tar","Sar","Ale","The","Tor","Sky"))]
+
+distances_dist <- as.dist(t(distances))
+
+dbMEM <- adespatial::dbmem(distances_dist, MEM.autocor="non-null", store.listw=T)
+write.csv(dbMEM,"Output/dbMEM.csv")
