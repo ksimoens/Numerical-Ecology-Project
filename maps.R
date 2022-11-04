3a404dc25db3bd272c2df9099ec6175068df7f82
ksimoens <kobe.simoens@ugent.be>
2022-11-04 23:18:44 +0100

    minor changes in maps

--- maps.R
+++ maps.R
@@ -95,18 +95,18 @@ env.sel <- env[,c(1,2,3)]
 env.coord <- cbind(coord,env.sel)
 
 mapSSTMean <- map + geom_point(data=env.coord,aes(x=lon,y=lat,col=SST_mean),size=3) + scale_color_viridis_c(option='magma') +
-              guides(col=guide_colourbar(title="mean SST"))
+              guides(col=guide_colourbar(title="SST mean (°C)"))
 mapSSTMean <- ggdraw() +
   draw_plot(mapSSTMean) +
-  draw_plot(inset, x = 0.491, y = -0.261,width=0.3)
+  draw_plot(inset, x = 0.460, y = -0.255,width=0.3)
 
 ggsave("mapSSTMean.png",mapSSTMean,device="png",width=15,height=15,units='cm')
 
 mapSSTRange <- map + geom_point(data=env.coord,aes(x=lon,y=lat,col=SST_range),size=3) + scale_color_viridis_c(option='magma') +
-              guides(col=guide_colourbar(title="SST range"))
+              guides(col=guide_colourbar(title="SST range (°C)"))
 mapSSTRange <- ggdraw() +
   draw_plot(mapSSTRange) +
-  draw_plot(inset, x = 0.491, y = -0.261,width=0.3)
+  draw_plot(inset, x = 0.459, y = -0.255,width=0.3)
 
 ggsave("mapSSTRange.png",mapSSTRange,device="png",width=15,height=15,units='cm')
 
@@ -116,7 +116,7 @@ mapSALMean <- ggdraw() +
   draw_plot(mapSALMean) +
   draw_plot(inset, x = 0.4765, y = -0.261,width=0.3)
 
-ggsave("mapSALMean.png",mapSALMean,device="png",width=15,height=15,units='cm')
+#ggsave("mapSALMean.png",mapSALMean,device="png",width=15,height=15,units='cm')
 
 lin <- read.csv("Output/PCoSpatial.csv",header=T,row.names=1)[,1:2]
 lin <- lin[match(rownames(coord),rownames(env)),]
@@ -129,7 +129,7 @@ mapLin1 <- ggdraw() +
   draw_plot(mapLin1) +
   draw_plot(inset, x = 0.477, y = -0.261,width=0.3)
 
-ggsave("mapLin1.png",mapLin1,device="png",width=15,height=15,units='cm')
+#ggsave("mapLin1.png",mapLin1,device="png",width=15,height=15,units='cm')
 
 mapLin2 <- map + geom_point(data=lin.coord,aes(x=lon,y=lat,col=PCo2),size=3) + scale_color_viridis_c(option='magma') +
               guides(col=guide_colourbar(title="spatial PCo2"))
@@ -137,7 +137,7 @@ mapLin2 <- ggdraw() +
   draw_plot(mapLin2) +
   draw_plot(inset, x = 0.478, y = -0.261,width=0.3)
 
-ggsave("mapLin2.png",mapLin2,device="png",width=15,height=15,units='cm')
+#ggsave("mapLin2.png",mapLin2,device="png",width=15,height=15,units='cm')
 
 AEM <- read.csv("Output/AEM.csv",header=T,row.names=1)
 AEM <- AEM[match(rownames(coord),rownames(AEM)),]
@@ -150,63 +150,63 @@ mapAEM1 <- ggdraw() +
   draw_plot(mapAEM1) +
   draw_plot(inset, x = 0.5107, y = -0.261,width=0.3)
 
-ggsave("mapAEM1.png",mapAEM1,device="png",width=15,height=15,units='cm')
+#ggsave("mapAEM1.png",mapAEM1,device="png",width=15,height=15,units='cm')
 
 mapAEM10 <- map + geom_point(data=AEM.coord,aes(x=lon,y=lat,col=AEM10),size=3) + scale_color_viridis_c(option='magma')
 mapAEM10 <- ggdraw() +
   draw_plot(mapAEM10) +
   draw_plot(inset, x = 0.505, y = -0.261,width=0.3)
 
-ggsave("mapAEM10.png",mapAEM10,device="png",width=15,height=15,units='cm')
+#ggsave("mapAEM10.png",mapAEM10,device="png",width=15,height=15,units='cm')
 
 mapAEM12 <- map + geom_point(data=AEM.coord,aes(x=lon,y=lat,col=AEM12),size=3) + scale_color_viridis_c(option='magma')
 mapAEM12 <- ggdraw() +
   draw_plot(mapAEM12) +
   draw_plot(inset, x = 0.505, y = -0.261,width=0.3)
 
-ggsave("mapAEM12.png",mapAEM12,device="png",width=15,height=15,units='cm')
+#ggsave("mapAEM12.png",mapAEM12,device="png",width=15,height=15,units='cm')
 
 mapAEM15 <- map + geom_point(data=AEM.coord,aes(x=lon,y=lat,col=AEM15),size=3) + scale_color_viridis_c(option='magma')
 mapAEM15 <- ggdraw() +
   draw_plot(mapAEM15) +
   draw_plot(inset, x = 0.505, y = -0.261,width=0.3)
 
-ggsave("mapAEM15.png",mapAEM15,device="png",width=15,height=15,units='cm')
+#ggsave("mapAEM15.png",mapAEM15,device="png",width=15,height=15,units='cm')
 
 mapAEM17 <- map + geom_point(data=AEM.coord,aes(x=lon,y=lat,col=AEM17),size=3) + scale_color_viridis_c(option='magma')
 mapAEM17 <- ggdraw() +
   draw_plot(mapAEM17) +
   draw_plot(inset, x = 0.505, y = -0.261,width=0.3)
 
-ggsave("mapAEM17.png",mapAEM17,device="png",width=15,height=15,units='cm')
+#ggsave("mapAEM17.png",mapAEM17,device="png",width=15,height=15,units='cm')
 
 mapAEM18 <- map + geom_point(data=AEM.coord,aes(x=lon,y=lat,col=AEM18),size=3) + scale_color_viridis_c(option='magma')
 mapAEM18 <- ggdraw() +
   draw_plot(mapAEM18) +
   draw_plot(inset, x = 0.498, y = -0.261,width=0.3)
 
-ggsave("mapAEM18.png",mapAEM18,device="png",width=15,height=15,units='cm')
+#ggsave("mapAEM18.png",mapAEM18,device="png",width=15,height=15,units='cm')
 
 mapAEM19 <- map + geom_point(data=AEM.coord,aes(x=lon,y=lat,col=AEM19),size=3) + scale_color_viridis_c(option='magma')
 mapAEM19 <- ggdraw() +
   draw_plot(mapAEM19) +
   draw_plot(inset, x = 0.505, y = -0.261,width=0.3)
 
-ggsave("mapAEM19.png",mapAEM19,device="png",width=15,height=15,units='cm')
+#ggsave("mapAEM19.png",mapAEM19,device="png",width=15,height=15,units='cm')
 
 mapAEM22 <- map + geom_point(data=AEM.coord,aes(x=lon,y=lat,col=AEM22),size=3) + scale_color_viridis_c(option='magma')
 mapAEM22 <- ggdraw() +
   draw_plot(mapAEM22) +
   draw_plot(inset, x = 0.505, y = -0.261,width=0.3)
 
-ggsave("mapAEM22.png",mapAEM22,device="png",width=15,height=15,units='cm')
+#ggsave("mapAEM22.png",mapAEM22,device="png",width=15,height=15,units='cm')
 
 mapAEM27 <- map + geom_point(data=AEM.coord,aes(x=lon,y=lat,col=AEM27),size=3) + scale_color_viridis_c(option='magma')
 mapAEM27 <- ggdraw() +
   draw_plot(mapAEM27) +
   draw_plot(inset, x = 0.505, y = -0.261,width=0.3)
 
-ggsave("mapAEM27.png",mapAEM27,device="png",width=15,height=15,units='cm')
+#ggsave("mapAEM27.png",mapAEM27,device="png",width=15,height=15,units='cm')
 
 coord <- read.csv("Input/coordinates.csv",header=T,row.names=1)
 
@@ -261,6 +261,17 @@ Sky <- as.numeric(coord[rownames(coord)=="Sky",1:2])
 TrotoSky <- shortestPath(trans, Tro, Sky, output="SpatialLines")
 TrotoSky <- SpatialLinesDataFrame(TrotoSky, data = data.frame(ID = 1))
 
+getColours <- function(pop){
+  col_key <- data.frame(
+      population = c("CHA","DEU","ESP","FRA","GRB","HEL","IRL","ITA","NDL","NOR","SVE","ATL","MED","SKA"),
+      colour = c(rep(c("#C7E020FF","#24868EFF","#440154FF"),3),c("#C7E020FF","#440154FF"),c("#C7E020FF","#24868EFF","#440154FF")),
+      shape = c(rep(15,3),rep(17,3),rep(19,3),18,18,15,17,19))
+
+  col_res <- col_key[col_key$population %in% unique(pop),]$colour
+  sha_res <- col_key[col_key$population %in% unique(pop),]$shape
+  return(list(colour = col_res,shape = sha_res))
+}
+
 lines <- ggplot() +
 	geom_sf(data=world) +
     coord_sf(xlim = c(-17,42), ylim = c(33,72), expand = FALSE) + 
@@ -272,8 +283,15 @@ lines <- ggplot() +
     annotation_north_arrow(location="tl", which_north = "true", 
         pad_x = unit(0.5, "cm"), pad_y = unit(1, "cm"),
         style = north_arrow_fancy_orienteering) +
-    geom_point(data=coord,aes(x=lon,y=lat,col=country),size=3) + 
-    scale_color_viridis_d(option='magma') + 
+    geom_point(data=coord,aes(x=lon,y=lat,col=country,shape=country),size=3) + 
+    scale_colour_manual(
+              name = "country",
+              labels = sort(unique(coord$country)),
+              values=getColours(coord$country)$colour) +
+             scale_shape_manual(
+              name = "country",
+              labels =sort(unique(coord$country)),
+              values=getColours(coord$country)$shape) + 
     geom_path(data=TrotoSky,aes(x=long,y=lat),size=1.5)
 
 ggsave("mapLine.png",lines,device='png',width=15,height=15,units='cm')
